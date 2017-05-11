0c2eba8e13f2fde344f287e44f3ea7adf1dc1072
Roma <detininroman@gmail.com>
2017-05-12 02:16:44 +0300

    little changes

--- pdp.c
+++ pdp.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 #include "pdp.h"
 
-word reg [NUMBER_OF_REGS] = {};
-byte mem [MEM_SIZE]       = {};
+word reg [REGS_NUM] = {};
+byte mem [MEM_SIZE] = {};
 
 #define sp reg [6]
 #define pc reg [7]
@@ -13,8 +13,9 @@ int N = 0;
 int Z = 0;
 
 struct SSDD ss, dd;
-
-word nn, r, xx;
+word nn = 0;
+word r  = 0;
+int  xx = 0;
 
 byte b_read (adr a)
 {
@@ -47,35 +48,30 @@ void load_file (FILE* file)
     unsigned int address  = 0;
     unsigned int cnt      = 0;
     unsigned int quantity = 0;
-    byte value            = 0;
+    unsigned int value    = 0;
 
-    while (fscanf (file, "%x %x", &address, &quantity) == 2)
+    while (fscanf (file, "%x%x", &address, &quantity) == 2)
     {
         for (cnt = 0; cnt < quantity; cnt ++)
         {
-            fscanf (file, "%hhx", &value);
-            b_write (address + cnt , value);
+            fscanf (file, "%x", &value);
+            b_write (address + cnt , (byte) value);
         }
     }
 }
 
-void mem_dump (adr start, word n)
+void dump ()
 {
-    for (int cnt = 0; cnt < n; cnt += 2)
+    printf ("\n\t\t\t\t\t");
+    for (int i = 0; i < 8; i++)
     {
-        assert (cnt % 2 == 0);
-        printf ("%.6o : %.6o\n",
-            start + cnt, w_read (start + cnt));
+        printf ("R[%d] = %o  ", i, reg[i]);
     }
 }
 
 void do_halt ()
 {
-    printf ("\n");
-    for (int i = 0; i < 8; i++)
-    {
-        printf ("R[%d] = %o\n", i, reg[i]);
-    }
+    dump ();
     exit (0);
 }
 
@@ -85,6 +81,7 @@ void do_sob ()
     {
         pc = pc - 2 * nn;
     }
+    dump ();
 }
 
 void do_clr ()
@@ -92,6 +89,7 @@ void do_clr ()
     reg [dd.a] = 0;
     N = 0;
     Z = 1;
+    dump ();
 }
 
 void do_br ()
@@ -110,21 +108,28 @@ void do_beq ()
 void do_mov ()
 {
     reg [dd.a] = ss.val;
-    //if (dd.place =
     change_state_flags (reg[dd.a]);
+    dump ();
+}
+
+void do_movb ()
+{
+    b_write (dd.a, ss.val);
+    dump ();
 }
 
 void do_add ()
 {
     reg [dd.a] = ss.val + dd.val;
     change_state_flags (reg[dd.a]);
+    dump ();
 }
 
 void do_unknown () {}
 
 void change_state_flags (word result)
 {
-    (result <  0)? N = 1: N = 0;
+    (result <  0)? N = 1: N = 0; // improve
     (result == 0)? Z = 1: Z = 0;
 }
 
@@ -133,61 +138,83 @@ struct SSDD get_mr (word w)
     struct SSDD res;
     int n = w & 7;
     int mode = (w >> 3) & 7;
+    word b = get_b (w >> 15);
 
     switch (mode)
     {
         case 0: //R1
-
+        {
             res.a   = n;
-            res.val = reg[n];
+            res.val = reg [n];
             printf ("R%d ", n);
             break;
-
+        }
         case 1: //(R1)
-
-            res.a   = reg [n];
-            res.val = w_read (res.a);
-            printf ("(R%d)  ", n);
+        {
+            res.a = reg [n];
+            res.val = (b == 1)? b_read (res.a): w_read (res.a);
+            printf ("(R%d) ", n);
             break;
-
+        }
         case 2: //(R1)+
-
-            res.a    = reg [n];
-            res.val  = w_read (res.a);
-            reg [n] += 2;
-            if (n == 7) printf ("#%o ", res.val);
-            else        printf ("(R%d)+ ", n);
+        {
+            res.a = reg [n];
+            if (b == 1)
+            {
+                res.val = b_read (res.a);
+                if (res.val >> 7 == 1)
+                {
+                    res.val = res.val | 0xFF00;
+                }
+                reg [n] += (n == 6 || n == 7)? 2: 1;
+            }
+            else
+            {
+                res.val = w_read (res.a);
+                reg [n] += 2;
+            }
+            (n == 7)? printf ("#%o ", res.val):
+                    printf ("(R%d)+ ", n);
             break;
-
+        }
         case 3: //@(R2)+
-
-            res.a   = w_read (reg [n]);
+        {
+            res.a   = w_read (reg[n]);
             res.val = w_read (res.a);
             reg [n] += 2;
-            if (n == 7) printf ("@#%o ", res.val);
-            else        printf ("@(R%d)+ ", n);
-            break;
 
+            (n == 7)? printf ("@#%o ", res.val):
+                    printf ("@(R%d)+ ", n);
+            break;
+        }
         case 4: //-(R2)
-
-            reg [n] += 2;
-            res.a    = reg [n];
-            res.val  = w_read (res.a);
-            if (n == 7) printf ("#%o ", res.val);
-            else        printf ("-(R%d) ", n);
+        {
+            res.a = reg [n];
+            if (b == 1)
+            {
+                res.val = b_read (res.a);
+                if (res.val >> 7 == 1)
+                {
+                    res.val = res.val | 0xFF00;
+                }
+                reg [n] -= (n == 6 || n == 7)? 2: 1;
+            }
+            else
+            {
+                res.val = w_read (res.a);
+                reg [n] -= 2;
+            }
+            printf ("-(R%d)+ ", n);
             break;
-
+        }
         case 5: //@-(R2)
-
-            reg [n] += 2;
-            res.a   = w_read (reg [n]);
+        {
+            reg [n] -= 2;
+            res.a   = w_read (reg[n]);
             res.val = w_read (res.a);
-            if (n == 7) printf ("@#%o ", res.val);
-            else        printf ("@-(R%d) ", n);
-            break;
-
-        default: printf ("Unknown mode\n"); exit (0);
-
+            printf ("@-(R%d ", n);
+        }
+        default: printf ("Unknown mode\n"); assert (0);
     }
     return res;
 }
@@ -222,39 +249,31 @@ void run_program ()
     }
 }
 
-word get_r (word w)
-{
-    return (w >> 6) & 7;
-}
+word get_r  (word w) { return (w >> 6) & 7; }
+word get_b  (word w) { return w & 1;        }
+word get_nn (word w) { return w & 0x3F;     }
 
-word get_nn (word w)
-{
-    return w & 077;
-}
-
-word get_xx (word w)
+int  get_xx (word w)
 {
     w = w & 0xFF;
-
-    if (w >> 7 == 1)
-    {
-        return -(0x100 - w);
-    }
-
-    else
-    {
-        return  w;
-    }
+    int ret_val = (w >> 7 == 1)? (w-0x100): w;
+    return ret_val;
 }
 
 int main (int argc, char *argv[])
 {
     FILE* file = fopen (argv[1], "r");
-    perror (argv[1]);
+    if (!argv [1])
+    {
+        printf ("You haven't entered the file!\n"); exit (1);
+    }
+    if (!file)
+    {
+        printf ("Cannot open the file!\n"); exit (2);
+    }
 
     load_file (file);
+    fclose(file);
     run_program ();
-    fclose (file);
-
     return 0;
 }
--- pdp.h
+++ pdp.h
@@ -1,13 +1,13 @@
 #ifndef PDP_H
 #define PDP_H
 
-typedef unsigned char      byte;
-typedef unsigned short int word;
-typedef unsigned int        adr;
+typedef unsigned char  byte;
+typedef unsigned short word;
+typedef unsigned int    adr;
 
 enum
 {
-    NUMBER_OF_REGS = 8,
+    REGS_NUM = 8,
     MEM_SIZE = 64 * 1024
 };
 
@@ -18,13 +18,8 @@ enum
     HAS_DD   = 1<<1,
     HAS_R    = 1<<2,
     HAS_NN   = 1<<3,
-    HAS_XX   = 1<<4
-};
-
-enum
-{
-    MEM,
-    REG
+    HAS_B    = 1<<4,
+    HAS_XX   = 1<<5
 };
 
 struct Command
@@ -40,7 +35,6 @@ struct SSDD
 {
     word val;
     adr a;
-    word place;
 };
 
 byte b_read             (adr a);
@@ -48,14 +42,16 @@ word w_read             (adr a);
 void b_write            (adr a, byte val);
 void w_write            (adr a, word val);
 void load_file          (FILE* file);
-void mem_dump           (adr start, word n);
 void change_state_flags (word result);
+word get_b              (word w);
 word get_r              (word w);
 word get_nn             (word w);
-word get_xx             (word w);
+int  get_xx             (word w);
+void dump               ();
 void do_halt            ();
 void do_add             ();
 void do_mov             ();
+void do_movb            ();
 void do_sob             ();
 void do_clr             ();
 void do_beq             ();
@@ -64,15 +60,15 @@ void do_unknown         ();
 void run_program        ();
 
 struct Command command_list[] = {
-    {0xFFFF, 0,         "HALT", do_halt,         NO_PARAM},
-    {0xF000, 0010000,    "MOV", do_mov,   HAS_SS | HAS_DD},
-    {0xF000, 0110000,   "MOVB", do_mov,   HAS_SS | HAS_DD},
-    {0xF000, 0060000,    "ADD", do_add,   HAS_SS | HAS_DD},
-    {0xFE00, 0077000,    "SOB", do_sob,   HAS_R  | HAS_NN},
-    {0xFFC0, 0005000,    "CLR", do_clr,            HAS_DD},
-    {0xFF00, 0001400,    "BEQ", do_beq,            HAS_XX},
-    {0xFF00, 0000400,     "BR", do_br,             HAS_XX},
-    {0,      0,      "unknown", do_unknown,      NO_PARAM}
+    {0xFFFF, 0,       "HALT", do_halt,                NO_PARAM},
+    {0xF000, 0010000,  "MOV", do_mov,  HAS_SS | HAS_DD | HAS_B},
+    {0xF000, 0110000, "MOVB", do_movb, HAS_SS | HAS_DD | HAS_B},
+    {0xF000, 0060000,  "ADD", do_add,          HAS_SS | HAS_DD},
+    {0xFE00, 0077000,  "SOB", do_sob,          HAS_R  | HAS_NN},
+    {0xFFC0, 0005000,  "CLR", do_clr,                   HAS_DD},
+    {0xFF00, 0001400,  "BEQ", do_beq,                   HAS_XX},
+    {0xFF00, 0000400,   "BR", do_br,                    HAS_XX},
+    {0,      0,    "unknown", do_unknown,             NO_PARAM}
 };
 
 #endif
